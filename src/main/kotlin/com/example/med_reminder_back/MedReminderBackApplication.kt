10c806cea015c9cd8904ada36505eda393619aa6
20Anton19 <lebedew.anton2018@yandex.ru>
2025-11-04 16:02:18 +0400

    Initial commit

---
+++ src/main/kotlin/com/example/med_reminder_back/MedReminderBackApplication.kt
@@ -0,0 +1,11 @@
+package com.example.med_reminder_back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MedReminderBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<MedReminderBackApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/med_reminder_back/MedReminderBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.med_reminder_back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MedReminderBackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
